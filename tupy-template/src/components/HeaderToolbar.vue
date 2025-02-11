02454c28365e74bec5759409546840faa58c06f5
Lucas <lucascaladob@gmail.com>
2025-02-11 15:00:15 -0300

    feat: header toolbar modular component

--- tupy-template/src/components/HeaderToolbar.vue
+++ tupy-template/src/components/HeaderToolbar.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="container">
-    <v-navigation-drawer v-model="drawer" :rail="rail" permanent>
-      <v-list-item :style="listItemStyle" nav>
+  <div class="header-toolbar">
+    <v-navigation-drawer v-model="drawer" :rail="rail" permanent style="min-width: 70px;">
+      <v-list-item :style="listItemStyle" nav height="75px"
+        style="display: flex; justify-content: center; align-items: center; position: relative;">
         <template v-slot:prepend>
-          <img :src="Logo" alt="Logo" />
+          <img :src="Logo" alt="Logo" style="height: 75px;" />
         </template>
+
       </v-list-item>
+      <div class="toolbar-button">
+          <v-btn icon="mdi-chevron-left" @click.stop="toggleRail"></v-btn>
+        </div>
       <v-divider></v-divider>
       <v-expansion-panels>
         <v-expansion-panel @click="rail = false">
@@ -26,64 +31,85 @@
   </div>
 </template>
 
+
 <script lang="ts">
 import { CSSProperties } from 'vue';
 import { Vue, Options } from 'vue-class-component';
 import LogoTupy from '../assets/images/LogoTupy.png';
 
 @Options({
-    props: {
-        rail: Boolean
-    }
+  props: {
+    rail: Boolean
+  }
 })
 export default class HeaderToolbar extends Vue {
-    Logo: string = LogoTupy;
-    drawer = true;
-    rail = false;
+  Logo: string = LogoTupy;
+  drawer = true;
+  rail = false;
 
-    get listItemStyle(): CSSProperties {
-        return {
-            width: this.rail ? '90px' : 'auto',
-            transition: 'width 0.3s',
-            color: '#0f4279'
-        };
-    }
+  get listItemStyle(): CSSProperties {
+    return {
+      width: this.rail ? '120px' : 'auto',
+      transition: 'width 0.3s',
+      color: '#0f4279'
+    };
+  }
 
-    get logoStyle(): CSSProperties {
-        return {
-            height: this.rail ? '75px' : '75px',
-            objectFit: 'cover',
-            transition: 'width 0.3s'
-        };
-    }
+  get logoStyle(): CSSProperties {
+    return {
+      height: this.rail ? '75px' : '75px',
+      objectFit: 'cover',
+      transition: 'width 0.3s'
+    };
+  }
 
-    get dataToolBar(): CSSProperties {
-        return {
-            display: this.rail ? 'none' : 'flex'
-        };
-    }
+  get dataToolBar(): CSSProperties {
+    return {
+      display: this.rail ? 'none' : 'flex'
+    };
+  }
+  toggleRail() {
+    this.rail = !this.rail
+  }
 }
 </script>
 
 <style>
+.header-toolbar {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  z-index: 999;
+}
+
+.toolbar-button {
+  position: absolute;
+  left: 100%;
+  top: 2%;
+  transform: translateX(-25%);
+  z-index: 9999;
+  /* Ajuste o estilo conforme necessário */
+}
+
 .container {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
+  display: flex;
+  flex-direction: column;
+  /*height: 100vh;*/
 }
 
 .header {
-    height: 75px; /* Defina a altura desejada para o header */
+  height: 75px;
+  /* Defina a altura desejada para o header */
 }
 
 .header-toolbar {
-    /*height: 0px;*/ /* Defina a altura desejada para o HeaderToolbar */
-    color: #000;
+  height: 75px;
+  /* Defina a altura desejada para o HeaderToolbar */
+  color: #000;
 }
-
 .content {
-    flex: 1;
-    overflow: auto;
-    height: calc(100vh - 125px);
+  flex: 1;
+  overflow: auto;
+  height: calc(100vh - 75px);
 }
 </style>
