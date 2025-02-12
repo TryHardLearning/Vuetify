0cf2858f2554c79ddbf7c2c4e80d201a56bcc83e
Lucas <lucascaladob@gmail.com>
2025-02-12 13:53:01 -0300

    feat: criação de componente footer

---
+++ tupy-template/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container fluid>
+      <v-footer border
+        style="
+          justify-content: space-between;
+          font-size: small;
+          font-weight: 400;
+          font-family: Roboto;
+          color: #757575;
+        "
+        ><label>Tupy S.A</label><label><v-icon>mdi-copyright</v-icon>Todos os diretos reservados</label>
+      </v-footer>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+
+@Options({
+  props: {
+  }
+})
+export default class Footer extends Vue {
+
+}
+</script>
