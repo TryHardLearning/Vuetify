6b1fe3d8a73927ea7d354f9ac220a6d04df35f7c
Lucas <lucascaladob@gmail.com>
2025-02-12 13:53:47 -0300

    feat: criar nova pagina para a edição dos dados

---
+++ tupy-template/src/pages/FormEdit.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-app>
+        <v-main fluid>
+            <Header :rail="rail" />
+            <div style="display: flex; flex-direction: row; justify-content: space-between;">
+                <HeaderToolbar :rail="rail" :class="{ 'toolbar-expanded': !rail, 'toolbar-collapsed': rail }" />
+                <EditPanel />
+            </div>
+            <Footer />
+        </v-main>
+    </v-app>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Header from '@/components/Header.vue';
+import HeaderToolbar from '@/components/HeaderToolbar.vue';
+import EditPanel from '@/components/EditPanel.vue';
+import Footer from '@/components/Footer.vue';
+
+@Options({
+    name: 'FormEdit',
+    components: {
+        Header,
+        HeaderToolbar,
+        EditPanel,
+        Footer
+    }
+})
+export default class FormEdit extends Vue {
+}
+</script>
+
+<style></style>
